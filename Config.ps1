32062cc8b5fd31a2627a6afcb9b72c5d9165b928
AlCpwnd <107629892+AlCpwnd@users.noreply.github.com>
2022-06-20 17:30:17 +0200

    Update Config.ps1

--- Config.ps1
+++ Config.ps1
@@ -4,3 +4,4 @@ Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name Terminal-Icons
 winget install JanDeDobbeleer.OhMyPosh
 winget install --id Git.Git -e --source winget
+winget install -e --id Microsoft.VisualStudioCode
