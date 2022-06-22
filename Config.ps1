d8e8dacac3621ffe81af7b036a3ed4ab87bc6372
AlCpwnd <a.dejaeger@hotmail.com>
2022-06-22 18:29:36 +0200

    Quick update of the config.ps1 file

--- Config.ps1
+++ Config.ps1
@@ -4,5 +4,9 @@ Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name Terminal-Icons
 Install-Module CredentialManager
 winget install JanDeDobbeleer.OhMyPosh
-winget install --id Git.Git -e --source winget
-winget install -e --id Microsoft.VisualStudioCode
+
+$TerminalConfigFile = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\Settings.json"
+$config = Get-Content $TerminalConfigFile | ConvertFrom-Json
+$config.Profiles.defaults.font.face = "CaskaydiaCove NF"
+$config.Profiles.defaults.font.size = 10
+ConvertTo-Json $config -Depth 32 | Set-Content $TerminalConfigFile
