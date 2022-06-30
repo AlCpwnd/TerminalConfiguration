7f3708eae4dd99a8fa478e4671eae7ee04f0365d
AlCpwnd <a.dejaeger@hotmail.com>
2022-06-30 15:13:51 +0200

    Updated the configuration to create profile based on PSProfile.ps1

--- Config.ps1
+++ Config.ps1
@@ -1,12 +1,18 @@
 #Requires -RunAsAdmin
 
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name Terminal-Icons
+if((Get-PSRepository -Name PSGallery).InstallationPolicy -ne "Trusted"){
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+}
+Install-Module -Name Terminal-Iclons
 Install-Module CredentialManager
 winget install JanDeDobbeleer.OhMyPosh
+$WebRequest = Invoke-WebRequest https://raw.githubusercontent.com/AlCpwnd/TerminalConfiguration/main/PSProfile.ps1
+$WebRequest.Content | Out-File $PROFILE
 
-$TerminalConfigFile = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\Settings.json"
-$config = Get-Content $TerminalConfigFile | ConvertFrom-Json
-$config.Profiles.defaults.font.face = "CaskaydiaCove NF"
-$config.Profiles.defaults.font.size = 10
-ConvertTo-Json $config -Depth 32 | Set-Content $TerminalConfigFile
+<#
+    $TerminalConfigFile = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\Settings.json"
+    $config = Get-Content $TerminalConfigFile | ConvertFrom-Json
+    $config.Profiles.defaults.font.face = "CaskaydiaCove NF"
+    $config.Profiles.defaults.font.size = 10
+    ConvertTo-Json $config -Depth 32 | Set-Content $TerminalConfigFile
+#>
