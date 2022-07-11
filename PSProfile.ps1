cc769b198879aabdea9faf36868e11edfd124ac7
Alexis De Jaeger <alexis.dejaeger@bkm.be>
2022-07-11 14:21:00 +0200

    Corrected typo

--- PSProfile.ps1
+++ PSProfile.ps1
@@ -1,2 +1,2 @@
 Import-Module -Name Terminal-Icons
-oh-my-posh init pwsh --config https://raw.githubusercontent.com/AlCpwnd/TerminalConfiguration/main/ADJ.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config https://raw.githubusercontent.com/AlCpwnd/TerminalConfiguration/main/ADJ.json | Invoke-Expression
