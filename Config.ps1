3a2b1bdfdc3fff771d15e8c07f51c74f6f3552ac
Alexis De Jaeger <alexis.dejaeger@bkm.be>
2022-07-11 14:15:04 +0200

    Corrected typo
    Corrected the installation command for Terminal Icons

--- Config.ps1
+++ Config.ps1
@@ -3,7 +3,7 @@
 if((Get-PSRepository -Name PSGallery).InstallationPolicy -ne "Trusted"){
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 }
-Install-Module -Name Terminal-Iclons
+Install-Module -Name Terminal-Icons
 Install-Module CredentialManager
 winget install JanDeDobbeleer.OhMyPosh
 $WebRequest = Invoke-WebRequest https://raw.githubusercontent.com/AlCpwnd/TerminalConfiguration/main/PSProfile.ps1
