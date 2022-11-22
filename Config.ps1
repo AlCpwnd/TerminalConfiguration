cc9c4d55c4395df91e9c180f3486a99e3f651f04
AlCpwnd <a.dejaeger@hotmail.com>
2022-11-22 19:46:43 +0000

    Updated the the configuration script and the README:
    - Config.ps1
      > Added terminal output to show the status of the script
      > Streamlined the PowerShell profile creation
      > Cleaned up the OhMyPosh installation
    - README.md
      > Cleaned up the documentation
      > Added links to the various sources

--- Config.ps1
+++ Config.ps1
@@ -1,10 +1,24 @@
 #Requires -RunAsAdmin
 
+Write-Host "> Verifying PSGallery repository status"
 if((Get-PSRepository -Name PSGallery).InstallationPolicy -ne "Trusted"){
+    Write-Host "+ Set PSGallery as a trusted repository"
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 }
-Install-Module -Name Terminal-Icons
-Install-Module CredentialManager
-winget install JanDeDobbeleer.OhMyPosh
-$WebRequest = Invoke-WebRequest https://raw.githubusercontent.com/AlCpwnd/TerminalConfiguration/main/PSProfile.ps1
-$WebRequest.Content | Out-File $PROFILE
+
+$Modules = "Terminal-Icons","CredentialManager"
+foreach($Module in $Modules){
+    if(!(Get-Module -Name $Module -ListAvailable)){
+        try{
+            Write-Host "> Installing module `'$Module`'"
+            Install-Module -Name $Module -ErrorAction Stop
+            Write-Host "+ Module `'$Module`' installed"
+        }catch{
+            Write-Host "! Failed to install module `'$Module`'"
+        }
+    }else{
+        Write-Host "> Updating module `'$Module`'"
+    }
+}
+winget install -exact --id JanDeDobbeleer.OhMyPosh --accept-source-agreements --accept-package-agreements
+Invoke-WebRequest https://raw.githubusercontent.com/AlCpwnd/TerminalConfiguration/main/PSProfile.ps1 -OutFile $PROFILE
