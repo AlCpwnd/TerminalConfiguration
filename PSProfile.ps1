1dc82509e0c81d61c55e0ca03e86e96fe0c77f20
AlCpwnd <a.dejaeger@hotmail.com>
2023-02-10 16:16:55 +0100

    update

--- PSProfile.ps1
+++ PSProfile.ps1
@@ -1,2 +1,3 @@
 Import-Module -Name Terminal-Icons
-oh-my-posh init pwsh --config https://raw.githubusercontent.com/AlCpwnd/TerminalConfiguration/main/ADJ.json | Invoke-Expression
+# oh-my-posh init pwsh --config https://raw.githubusercontent.com/AlCpwnd/TerminalConfiguration/main/ADJ.json | Invoke-Expression
+oh-my-posh init pwsh --config C:\Users\JAAL\Git\TerminalConfiguration\ADJ.json | Invoke-Expression
