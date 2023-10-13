6e62c1c220e59f81e8395f0dc744fbca04a5295a
AlCpwnd <a.dejaeger@hotmail.com>
2023-10-13 14:55:45 +0200

    Script cleanup and updated

--- Config.ps1
+++ Config.ps1
@@ -1,25 +1,25 @@
 #Requires -RunAsAdmin
 
-Write-Host "> Verifying PSGallery repository status"
-if((Get-PSRepository -Name PSGallery).InstallationPolicy -ne "Trusted"){
-    Write-Host "+ Set PSGallery as a trusted repository"
+# Defining PSGallery as a trusted repository.
+if((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted'){
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 }
 
-$Modules = "Terminal-Icons","CredentialManager"
-foreach($Module in $Modules){
-    if(!(Get-Module -Name $Module -ListAvailable)){
-        try{
-            Write-Host "> Installing module `'$Module`'"
-            Install-Module -Name $Module -ErrorAction Stop
-            Write-Host "+ Module `'$Module`' installed"
-        }catch{
-            Write-Host "! Failed to install module `'$Module`'"
-        }
-    }else{
-        Write-Host "> Updating module `'$Module`'"
-        Update-Module -Name $Module
-    }
+# Installing package provider.
+$PackageProviders = 'NuGet'
+foreach($PackageProvider in $PackageProviders){
+    Install-PackageProvider -Name $PackageProvider -Force
 }
-winget install -exact --id JanDeDobbeleer.OhMyPosh --accept-source-agreements --accept-package-agreements
-Invoke-WebRequest https://raw.githubusercontent.com/AlCpwnd/TerminalConfiguration/main/PSProfile.ps1 -OutFile $PROFILE
+
+# Installing modules.
+$Modules = 'Terminal-Icons','PSWindowsUpdate'
+foreach($Module in $Modules){
+    Install-Module -Name $Module -Force
+}
+
+# Setting up PowerShell profile.
+## OhMyPosh installation.
+winget install --exact --id JanDeDobbeleer.OhMyPosh --accept-source-agreements --accept-package-agreements
+## Profile download from GitHub repository.
+$ProfileUrl = 'https://raw.githubusercontent.com/AlCpwnd/TerminalConfiguration/main/PSProfile.ps1'
+Invoke-WebRequest -Uri $ProfileUrl -UseBasicParsing -OutFile $PROFILE
