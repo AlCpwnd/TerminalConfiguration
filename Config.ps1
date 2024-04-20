a1dc59d696dc8de338d30ea0cecba2820b0be789
AlCapwnd <a.dejaeger@hotmail.com>
2024-04-21 00:13:25 +0200

    Added Windows terminal configuration file copy. Still untested

--- Config.ps1
+++ Config.ps1
@@ -1,4 +1,4 @@
-#Requires -RunAsAdmin
+#Requires -RunAsAdministrator
 
 # Defining PSGallery as a trusted repository.
 if((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted'){
@@ -23,3 +23,11 @@ winget install --exact --id JanDeDobbeleer.OhMyPosh --accept-source-agreements -
 ## Profile download from GitHub repository.
 $ProfileUrl = 'https://raw.githubusercontent.com/AlCpwnd/TerminalConfiguration/main/PSProfile.ps1'
 Invoke-WebRequest -Uri $ProfileUrl -UseBasicParsing -OutFile $PROFILE
+
+# Configures Windows Terminal settings.
+$SettingsPath = (Get-ChildItem -Path $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_*\LocalState\settings.json).FullName
+if($SettingsPath){
+    Copy-Item -Path .\setting.json -Destination $SettingsPath -Force
+}else{
+    Write-Host 'Failed to find existing settings file. Please configure it manually.' -ForegroundColor Red
+}
