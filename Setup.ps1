608327d50b68190bf640b06a277bec4d51087d0e
AlCpwnd <a.dejaeger@hotmail.com>
2026-07-20 17:59:46 +0200

    First big rewrite.

--- Setup.ps1
+++ Setup.ps1
@@ -1,36 +1,63 @@
 #Requires -RunAsAdministrator
 
+# Installing fonts.
+$fonts = Get-ChildItem -Path .\Font -Include *.ttf, *.otf
+$shell = New-Object -ComObject Shell.Application
+$fontsFolder = $shell.NameSpace(0x14)
+foreach($font in $fonts){
+    $fontsFolder.CopyHere($font.FullName)
+}
+
 # Defining PSGallery as a trusted repository.
-if((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted'){
+if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 }
 
 # Installing package provider.
-$PackageProviders = 'NuGet'
-foreach($PackageProvider in $PackageProviders){
+$PackageProviders = @('NuGet')
+foreach ($PackageProvider in $PackageProviders) {
     Install-PackageProvider -Name $PackageProvider -Force
 }
 
 # Installing modules.
-$Modules = 'Terminal-Icons','PSWindowsUpdate'
-foreach($Module in $Modules){
+$Modules = 'Terminal-Icons', 'PSWindowsUpdate'
+foreach ($Module in $Modules) {
     Install-Module -Name $Module -Force
 }
 
 # Copying VIM configuration
 Copy-Item .\.vimrc $HOME
 
+# Using winget to install required programs.
+$apps = 'JanDeDobbeleer.OhMyPosh','vim.vim'
+foreach($app in $apps){
+    winget install --exact --id $app --accept-source-agreements --accept-package-agreements
+}
+
 # Setting up PowerShell profile.
 ## OhMyPosh installation.
-winget install --exact --id JanDeDobbeleer.OhMyPosh --accept-source-agreements --accept-package-agreements
 ## Profile download from GitHub repository.
 $ProfileUrl = 'https://raw.githubusercontent.com/AlCpwnd/TerminalConfiguration/main/PSProfile.ps1'
 Invoke-WebRequest -Uri $ProfileUrl -UseBasicParsing -OutFile $PROFILE
 
 # Configures Windows Terminal settings.
 $SettingsPath = (Get-ChildItem -Path $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_*\LocalState\settings.json).FullName
-if($SettingsPath){
-    Copy-Item -Path .\setting.json -Destination $SettingsPath -Force
-}else{
-    Write-Host 'Failed to find existing settings file. Please configure it manually.' -ForegroundColor Red
+$settings = Get-Content -Path $SettingsPath | ConvertFrom-Json
+
+$themeFile = '.\Themes\Dracula.json' #File name containing the theme you want to apply.
+$theme = Get-Content -Path $themeFile | ConvertFrom-Json
+
+## Adding the theme to the terminal
+if (-not ($settings.schemes | Where-Object { $_.name -eq $theme.name } )) {
+    $settings.schemes += $theme
 }
+
+if (-not(Get-Member -InputObject $settings.profiles.defaults -Name 'colorScheme')) {
+    $settings.profiles.defaults | Add-Member -MemberType NoteProperty -Name 'colorScheme' -Value $($theme.name)
+}
+else {
+    $settings.profiles.defaults.colorScheme = $theme.name
+}
+
+$settingsContents = $settings | ConvertTo-Json -Depth 3
+$settingsContents | Out-File -FilePath $SettingsPath -Encoding utf8
